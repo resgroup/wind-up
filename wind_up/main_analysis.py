f0318f7cc6133b639b4f3e35adb52630fb3b87a0
Alex Clerc <alex.clerc@res-group.com>
2024-05-10 11:34:49 +0100

    Update main_analysis.py
    fix ruff issues

--- wind_up/main_analysis.py
+++ wind_up/main_analysis.py
@@ -144,6 +144,34 @@ def filter_ref_df_for_wake_free(
     return ref_df.drop(columns=["rounded_wd"])
 
 
+def filter_ref_df_for_wd_and_hod(ref_df: pd.DataFrame, ref_wd_col: str, cfg: WindUpConfig) -> pd.DataFrame:
+    ref_df = ref_df.copy()
+    if cfg.ref_wd_filter is not None:
+        rows_before = len(ref_df)
+        if cfg.ref_wd_filter[0] < cfg.ref_wd_filter[1]:
+            ref_df = ref_df[(ref_df[ref_wd_col] >= cfg.ref_wd_filter[0]) & (ref_df[ref_wd_col] <= cfg.ref_wd_filter[1])]
+        else:
+            ref_df = ref_df[(ref_df[ref_wd_col] >= cfg.ref_wd_filter[0]) | (ref_df[ref_wd_col] <= cfg.ref_wd_filter[1])]
+        rows_after = len(ref_df)
+        logger.info(
+            f"removed {rows_before - rows_after} [{100 * (rows_before - rows_after) / rows_before:.1f}%] "
+            f"rows from ref_df using ref_wd_filter",
+        )
+
+    if cfg.ref_hod_filter is not None:
+        rows_before = len(ref_df)
+        if cfg.ref_hod_filter[0] < cfg.ref_hod_filter[1]:
+            ref_df = ref_df[(ref_df.index.hour >= cfg.ref_hod_filter[0]) & (ref_df.index.hour <= cfg.ref_hod_filter[1])]
+        else:
+            ref_df = ref_df[(ref_df.index.hour >= cfg.ref_hod_filter[0]) | (ref_df.index.hour <= cfg.ref_hod_filter[1])]
+        rows_after = len(ref_df)
+        logger.info(
+            f"removed {rows_before - rows_after} [{100 * (rows_before - rows_after) / rows_before:.1f}%] "
+            f"rows from ref_df using ref_hod_filter",
+        )
+    return ref_df
+
+
 def get_ref_df(
     *,
     ref_name: str,
@@ -190,29 +218,7 @@ def get_ref_df(
         ref_df[ref_wd_col] = ref_df[original_wd_col]
         ref_df = add_fake_power_data(ref_df, ref_pw_col=ref_pw_col, ref_ws_col=ref_ws_col, scada_pc=scada_pc)
 
-    if cfg.ref_wd_filter is not None:
-        rows_before = len(ref_df)
-        if cfg.ref_wd_filter[0] < cfg.ref_wd_filter[1]:
-            ref_df = ref_df[(ref_df[ref_wd_col] >= cfg.ref_wd_filter[0]) & (ref_df[ref_wd_col] <= cfg.ref_wd_filter[1])]
-        else:
-            ref_df = ref_df[(ref_df[ref_wd_col] >= cfg.ref_wd_filter[0]) | (ref_df[ref_wd_col] <= cfg.ref_wd_filter[1])]
-        rows_after = len(ref_df)
-        logger.info(
-            f"removed {rows_before - rows_after} [{100 * (rows_before - rows_after) / rows_before:.1f}%] "
-            f"rows from ref_df using ref_wd_filter",
-        )
-
-    if cfg.ref_hod_filter is not None:
-        rows_before = len(ref_df)
-        if cfg.ref_hod_filter[0] < cfg.ref_hod_filter[1]:
-            ref_df = ref_df[(ref_df.index.hour >= cfg.ref_hod_filter[0]) & (ref_df.index.hour <= cfg.ref_hod_filter[1])]
-        else:
-            ref_df = ref_df[(ref_df.index.hour >= cfg.ref_hod_filter[0]) | (ref_df.index.hour <= cfg.ref_hod_filter[1])]
-        rows_after = len(ref_df)
-        logger.info(
-            f"removed {rows_before - rows_after} [{100 * (rows_before - rows_after) / rows_before:.1f}%] "
-            f"rows from ref_df using ref_hod_filter",
-        )
+    ref_df = filter_ref_df_for_wd_and_hod(ref_df, ref_wd_col=ref_wd_col, cfg=cfg)
 
     if cfg.require_test_wake_free or cfg.require_ref_wake_free:
         ref_df = filter_ref_df_for_wake_free(
