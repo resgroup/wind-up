9815c4189ff6e2b67f974329ffd09947dd4235f9
Alex Clerc <alex.clerc@res-group.com>
2024-06-26 16:01:50 +0100

    Update combine_results_plots.py
    use confidence interval for error bars

--- wind_up/plots/combine_results_plots.py
+++ wind_up/plots/combine_results_plots.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.stats import norm
 
 from wind_up.models import PlotConfig
 
 
-def plot_combine_results(trdf: pd.DataFrame, tdf: pd.DataFrame, plot_cfg: PlotConfig) -> None:
+def plot_combine_results(trdf: pd.DataFrame, tdf: pd.DataFrame, plot_cfg: PlotConfig, confidence: float = 0.9) -> None:
     show_plots = plot_cfg.show_plots
     save_plots = plot_cfg.save_plots
+    z_score = norm.ppf((1 + confidence) / 2)
 
     plt.figure(figsize=(10, 6))
     labels = [
@@ -18,14 +20,14 @@ def plot_combine_results(trdf: pd.DataFrame, tdf: pd.DataFrame, plot_cfg: PlotCo
         )
     ]
     values = trdf["uplift_frc"] * 100
-    yerrs = trdf["unc_one_sigma_frc"] * 100
+    yerrs = trdf["unc_one_sigma_frc"] * 100 * z_score
     plt.bar(labels, values, yerr=yerrs, capsize=3)
     plt.xlabel("turbine")
-    plt.ylabel("uplift [%] and 1-sigma uncertainty")
-    plot_title = "uplift by test-ref pair"
+    plt.ylabel("uplift [%]")
+    plot_title = f"uplift and {confidence*100:.0f}% CI by test-ref pair"
     plt.title("uplift by test-ref pair")
     plt.grid(axis="y")
-    plt.xticks(rotation=45, ha="right")
+    plt.xticks(rotation=90, ha="right")
     plt.tight_layout()
     if show_plots:
         plt.show()
@@ -35,14 +37,14 @@ def plot_combine_results(trdf: pd.DataFrame, tdf: pd.DataFrame, plot_cfg: PlotCo
     plt.figure()
     labels = tdf["test_wtg"]
     values = tdf["p50_uplift"] * 100
-    yerrs = tdf["sigma"] * 100
+    yerrs = tdf["sigma"] * 100 * z_score
     plt.bar(labels, values, yerr=yerrs, capsize=3)
     plt.xlabel("turbine")
-    plt.ylabel("uplift [%] and 1-sigma uncertainty")
-    plot_title = "combined uplift"
+    plt.ylabel("uplift [%]")
+    plot_title = f"combined uplift and {confidence*100:.0f}% CI"
     plt.title(plot_title)
     plt.grid(axis="y")
-    plt.xticks(rotation=45, ha="right")
+    plt.xticks(rotation=90, ha="right")
     plt.tight_layout()
     if show_plots:
         plt.show()
