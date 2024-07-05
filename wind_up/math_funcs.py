b923d7281e76fc4dc958093617a6b9b637042dc5
Paul <paul.fleming@nrel.gov>
2024-07-05 14:59:34 -0600

    Revise circ_diff function

--- wind_up/math_funcs.py
+++ wind_up/math_funcs.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 
-def circ_diff(angle1: float | np.generic, angle2: float | np.generic) -> float | np.generic:
-    angle1_rad = np.radians(angle1)
-    angle2_rad = np.radians(angle2)
-    sin_angle1 = np.sin(angle1_rad)
-    cos_angle1 = np.cos(angle1_rad)
-    sin_angle2 = np.sin(angle2_rad)
-    cos_angle2 = np.cos(angle2_rad)
-    denominator = cos_angle2 * cos_angle2 + sin_angle2 * sin_angle2
-    temp1 = (cos_angle1 * cos_angle2 + sin_angle1 * sin_angle2) / denominator
-    temp2 = (sin_angle1 * cos_angle2 - cos_angle1 * sin_angle2) / denominator
-    return np.degrees(np.arctan2(temp2, temp1))
+def circ_diff(angle1: float | np.generic | list, angle2: float | np.generic | list) -> float | np.generic:
+    """Calculate the circular difference between two angles.
+
+    Args:
+        angle1: First angle in degrees.
+        angle2: Second angle in degrees.
+
+    Returns:
+        Circular difference between the two angles in degrees
+    """
+
+    # Convert list to numpy array
+    if isinstance(angle1, list):
+        angle1 = np.array(angle1)
+    if isinstance(angle2, list):
+        angle2 = np.array(angle2)
+
+    return np.mod(angle1 - angle2 + 180.0, 360.0) - 180.0
