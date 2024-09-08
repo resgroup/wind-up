2d4a8c625a848d78552519dd97ad583bdd103ac6
Alex Clerc <alex.clerc@res-group.com>
2024-09-08 16:30:06 +0100

    wedowind_example.py WIP

--- examples/wedowind_example.py
+++ examples/wedowind_example.py
@@ -1,3 +1,5 @@
+# example based on https://relight.cloud/doc/turbine-upgrade-dataset-9zw1vl/turbineperformance
+
 import logging
 import math
 import sys
@@ -6,14 +8,20 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from wind_up.constants import OUTPUT_DIR, PROJECTROOT_DIR, TIMESTAMP_COL, DataColumns
+from wind_up.interface import AssessmentInputs
+from wind_up.main_analysis import run_wind_up_analysis
+from wind_up.models import PlotConfig, WindUpConfig
+from wind_up.wind_funcs import calc_cp
 
 sys.path.append(str(PROJECTROOT_DIR))
 from examples.helpers import download_zenodo_data, setup_logger
 
 CACHE_DIR = PROJECTROOT_DIR / "cache" / "wedowind_example_data"
-ANALYSIS_OUTPUT_DIR = OUTPUT_DIR / "wedowind_example"
+ASSESSMENT_NAME = "wedowind_example"
+ANALYSIS_OUTPUT_DIR = OUTPUT_DIR / ASSESSMENT_NAME
 ANALYSIS_OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
 PARENT_DIR = Path(__file__).parent
@@ -38,19 +46,18 @@ def unpack_wedowind_scada(rated_power_kw: float) -> pd.DataFrame:
     )
     scada_df = pd.concat([scada_df_test, scada_df_ref])
     scada_df[DataColumns.active_power_mean] = scada_df["normalized_power"] * rated_power_kw
+    # map some mast data to the turbine for convenience
     scada_df[DataColumns.wind_speed_mean] = scada_df["V"]
     scada_df[DataColumns.yaw_angle_mean] = scada_df["D"]
+    # placeholder values for other required columns
     scada_df[DataColumns.pitch_angle_mean] = 0
     scada_df[DataColumns.gen_rpm_mean] = 1000
     scada_df[DataColumns.shutdown_duration] = 0
 
-    scada_df_mast = (
-        scada_df_raw.drop(columns=["y_ctrl(normalized)", "y_test(normalized)"]).copy().assign(TurbineName="Mast")
-    )
-    scada_df = pd.concat([scada_df, scada_df_mast])
-
     scada_df = scada_df.set_index("time")
     scada_df.index.name = TIMESTAMP_COL
+    # make index UTC
+    scada_df.index = scada_df.index.tz_localize("UTC")
     return scada_df
 
 
@@ -85,10 +92,62 @@ if __name__ == "__main__":
     )
 
     logger.info("Preprocessing turbine SCADA data")
-    assumed_rated_power_kw = 2000
+    assumed_rated_power_kw = 1500
+    rotor_diameter_m = 80
+    cutout_ws_mps = 20
     scada_df = unpack_wedowind_scada(rated_power_kw=assumed_rated_power_kw)
     metadata_df = make_wedowind_metadata_df()
 
+    # it is unclear how the scada data is related to the metadata so look for wakes in the data
+    make_custom_plots = False
+    if make_custom_plots:
+        (ANALYSIS_OUTPUT_DIR / "custom_plots").mkdir(exist_ok=True, parents=True)
+        for name, df in scada_df.groupby("TurbineName"):
+            for col in df.columns:
+                plt.figure()
+                plt.plot(df.index, df[col])
+                title = f"{name} {col}"
+                plt.xlabel(TIMESTAMP_COL)
+                plt.ylabel(col)
+                plt.xticks(rotation=90)
+                plt.grid()
+                plt.tight_layout()
+                plt.savefig(ANALYSIS_OUTPUT_DIR / "custom_plots" / f"{title}.png")
+                plt.close()
+
+        region2_df = scada_df[(scada_df["normalized_power"] > 0.2) & (scada_df["normalized_power"] < 0.8)]  # noqa PLR2004
+        for name, df in region2_df.groupby("TurbineName"):
+            if name == "Mast":
+                continue
+            # find mean normalized_power and V binned by D
+            _df = df.copy()
+            _df["D_bin"] = pd.cut(_df["D"], bins=range(0, 361, 5))
+            binned = _df.groupby("D_bin", observed=False)[["D", "normalized_power", "V"]].mean()
+            plt.figure()
+            plt.plot(
+                binned["D"],
+                calc_cp(
+                    power_kw=binned["normalized_power"] * assumed_rated_power_kw,
+                    ws_ms=binned["V"],
+                    air_density_kgpm3=1.2,
+                    rotor_diameter_m=rotor_diameter_m,
+                ),
+                marker=".",
+            )
+            title = f"{name} Cp vs D"
+            plt.title(title)
+            plt.xlabel("D")
+            plt.ylabel("normalized_power, V")
+            plt.xticks(rotation=90)
+            plt.grid()
+            plt.tight_layout()
+            plt.savefig(ANALYSIS_OUTPUT_DIR / "custom_plots" / f"{title}.png")
+            plt.close()
+
+    # based on the above I think the objects are MAST1, test=WT1 and ref=WT2
+    scada_df = scada_df.replace({"TurbineName": {"Test": "WT1", "Ref": "WT2", "Mast": "MAST1"}})
+    # drop everything except the turbines from the metadata
+    metadata_df = metadata_df[metadata_df["Name"].isin(["WT1", "WT2"])]
     # make up reanalysis for now
     from wind_up.reanalysis_data import ReanalysisDataset
 
@@ -104,3 +163,65 @@ if __name__ == "__main__":
             index=pd.DatetimeIndex(pd.date_range(start=scada_df.index.min(), periods=rows, freq="h", tz="UTC")),
         ),
     )
+
+    wtg_map = {
+        x: {
+            "name": x,
+            "turbine_type": {
+                "turbine_type": "unknown turbine type",
+                "rotor_diameter_m": rotor_diameter_m,
+                "rated_power_kw": assumed_rated_power_kw,
+                "cutout_ws_mps": cutout_ws_mps,
+                "normal_operation_pitch_range": (-10.0, 35.0),
+                "normal_operation_genrpm_range": (0, 2000.0),
+            },
+        }
+        for x in ["WT1", "WT2"]
+    }
+
+    cfg = WindUpConfig(
+        assessment_name=ASSESSMENT_NAME,
+        use_lt_distribution=False,
+        out_dir=OUTPUT_DIR / ASSESSMENT_NAME,
+        test_wtgs=[wtg_map[x] for x in ["WT1"]],
+        ref_wtgs=[wtg_map[x] for x in ["WT2"]],
+        analysis_first_dt_utc_start=scada_df.index.min(),
+        upgrade_first_dt_utc_start=scada_df[scada_df["upgrade status"] > 0].index.min(),
+        analysis_last_dt_utc_start=scada_df[scada_df["upgrade status"] > 0].index.max(),
+        years_offset_for_pre_period=1,
+        lt_first_dt_utc_start=scada_df.index.min(),
+        lt_last_dt_utc_start=scada_df.index.min() + pd.DateOffset(years=1) - pd.Timedelta(minutes=10),
+        detrend_first_dt_utc_start=scada_df.index.min(),
+        detrend_last_dt_utc_start=scada_df[scada_df["upgrade status"] > 0].index.min()
+        - pd.DateOffset(weeks=1)
+        - pd.Timedelta(minutes=10),
+        years_for_lt_distribution=1,
+        years_for_detrend=1,
+        ws_bin_width=1.0,
+        asset={
+            "name": "Mystery Wind Farm",
+            "wtgs": list(wtg_map.values()),
+        },
+        missing_scada_data_fields=["YawAngleMin", "YawAngleMax"],
+        prepost={
+            "pre_first_dt_utc_start": scada_df.index.min(),
+            "pre_last_dt_utc_start": scada_df[scada_df["upgrade status"] > 0].index.min()
+            - pd.DateOffset(weeks=1)
+            - pd.Timedelta(minutes=10),
+            "post_first_dt_utc_start": scada_df[scada_df["upgrade status"] > 0].index.min(),
+            "post_last_dt_utc_start": scada_df[scada_df["upgrade status"] > 0].index.max(),
+        },
+        optimize_northing_corrections=False,
+    )
+    msg = f"{cfg.out_dir=}"
+    logger.info(msg)
+    plot_cfg = PlotConfig(show_plots=False, save_plots=True, plots_dir=cfg.out_dir / "plots")
+    assessment_inputs = AssessmentInputs.from_cfg(
+        cfg=cfg,
+        plot_cfg=plot_cfg,
+        scada_df=scada_df[(scada_df["D"] < 70) | (scada_df["D"] > 150)],  # noqa PLR2004 filter out apparent mast waked sector
+        metadata_df=metadata_df,
+        reanalysis_datasets=[reanalysis_dataset],
+        cache_dir=CACHE_DIR,
+    )
+    results_per_test_ref_df = run_wind_up_analysis(assessment_inputs)
