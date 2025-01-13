4344e94abbbc3413442ba09063ccb2218cf1b6c7
Gabriele Calvo <gabriele.calvo@res-group.com>
2025-01-13 14:56:57 +0000

    speeding up check_windspeed_drift (test mean 980us -> 482us)

--- wind_up/windspeed_drift.py
+++ wind_up/windspeed_drift.py
@@ -13,39 +13,43 @@ if TYPE_CHECKING:
     from wind_up.models import PlotConfig, WindUpConfig
 
 
-def add_rolling_windspeed_diff(
-    wtg_df: pd.DataFrame, *, ws_col: str, reanalysis_ws_col: str, timebase_s: int
-) -> pd.DataFrame:
-    wtg_df = wtg_df.copy()
+def _calculate_rolling_windspeed_diff(
+    wtg_df: pd.DataFrame,
+    *,
+    ws_col: str,
+    reanalysis_ws_col: str,
+    timebase_s: int,
+    ws_ll: float = 6,
+    ws_ul: float = 15,
+    rolling_period: float = 90,
+    min_roll_days: float = 14,
+    min_rolling_coverage: float = 1 / 3,
+) -> pd.Series:
+    ws_diff_to_renalysis = wtg_df[ws_col] - wtg_df[reanalysis_ws_col]
+    ws_diff_to_renalysis.loc[(wtg_df[ws_col] < ws_ll) | (wtg_df[ws_col] > ws_ul)] = np.nan
 
-    # check for ws drift issue
-    wtg_df["ws_diff_to_renalysis"] = wtg_df[ws_col] - wtg_df[reanalysis_ws_col]
-    ws_ll = 6
-    ws_ul = 15
-    wtg_df.loc[wtg_df[ws_col] < ws_ll, "ws_diff_to_renalysis"] = np.nan
-    wtg_df.loc[wtg_df[ws_col] > ws_ul, "ws_diff_to_renalysis"] = np.nan
-    rolling_days = 90
     rows_per_day = 24 * 3600 / timebase_s
-    wtg_df["rolling_windspeed_diff"] = (
-        wtg_df["ws_diff_to_renalysis"]
-        .rolling(window=round(rolling_days * rows_per_day), min_periods=round(rolling_days * rows_per_day // 3))
-        .median()
-    )
-    min_roll_days = 14
-    while rolling_days >= (min_roll_days * 2) and len(wtg_df["rolling_windspeed_diff"].dropna()) == 0:
-        rolling_days = rolling_days // 2
-        wtg_df["rolling_windspeed_diff"] = (
-            wtg_df["ws_diff_to_renalysis"]
-            .rolling(window=round(rolling_days * rows_per_day), min_periods=round(rolling_days * rows_per_day // 3))
-            .median()
-        )
-    if len(wtg_df["rolling_windspeed_diff"].dropna()) == 0:
+
+    def _rolling_specs(rolling_period: float) -> dict[str, int]:
+        return {
+            "window": round(rolling_period * rows_per_day),
+            "min_periods": round(rolling_period * rows_per_day * min_rolling_coverage),
+        }
+
+    rolling_windspeed_diff = ws_diff_to_renalysis.rolling(**_rolling_specs(rolling_period)).median()
+
+    while rolling_period >= (min_roll_days * 2) and len(rolling_windspeed_diff.dropna()) == 0:
+        rolling_period = rolling_period // 2
+        rolling_windspeed_diff = ws_diff_to_renalysis.rolling(**_rolling_specs(rolling_period)).median()
+
+    if len(rolling_windspeed_diff.dropna()) == 0:
         result_manager.warning("could not calculate rolling windspeed diff")
-    return wtg_df
+
+    return rolling_windspeed_diff
 
 
-def calc_max_abs_relative_rolling_windspeed_diff(wtg_df: pd.DataFrame) -> float:
-    return (wtg_df["rolling_windspeed_diff"] - wtg_df["rolling_windspeed_diff"].median()).abs().max()
+def calc_max_abs_relative_rolling_windspeed_diff(ser: pd.Series) -> float:
+    return (ser - ser.median()).abs().max()
 
 
 def check_windspeed_drift(
@@ -58,18 +62,18 @@ def check_windspeed_drift(
     plot_cfg: PlotConfig | None,
     sub_dir: str | None = None,
 ) -> tuple[float, float]:
-    wtg_df = wtg_df.copy()
-    wtg_df = add_rolling_windspeed_diff(
+    rolling_windspeed_diff = _calculate_rolling_windspeed_diff(
         wtg_df, ws_col=ws_col, reanalysis_ws_col=reanalysis_ws_col, timebase_s=cfg.timebase_s
     )
+
     if plot_cfg is not None:
         plot_rolling_windspeed_diff_one_wtg(
-            wtg_df=wtg_df, wtg_name=wtg_name, ws_col=ws_col, plot_cfg=plot_cfg, sub_dir=sub_dir
+            ser=rolling_windspeed_diff, wtg_name=wtg_name, ws_col=ws_col, plot_cfg=plot_cfg, sub_dir=sub_dir
         )
 
-    max_abs_rel_diff = calc_max_abs_relative_rolling_windspeed_diff(wtg_df)
+    max_abs_rel_diff = calc_max_abs_relative_rolling_windspeed_diff(rolling_windspeed_diff)
     max_abs_rel_diff_pp_period = calc_max_abs_relative_rolling_windspeed_diff(
-        wtg_df.loc[cfg.analysis_first_dt_utc_start : cfg.analysis_last_dt_utc_start],  # type: ignore[misc]
+        rolling_windspeed_diff.loc[cfg.analysis_first_dt_utc_start : cfg.analysis_last_dt_utc_start],  # type: ignore[misc]
     )
 
     ws_diff_ul = 1
