ec24bbfb668d8907f2b98b8d7b8fefb7c102c8f8
Gabriele Calvo <gabriele.calvo@res-group.com>
2025-03-28 10:53:27 +0000

    improved `download_zenodo_data`
    download_zenodo_data doesn't call zenodo if all files are cached.
    no SMARTEOLE specific code.
    removed unused filepaths list.

--- examples/helpers.py
+++ examples/helpers.py
@@ -20,15 +20,22 @@ logger = logging.getLogger(__name__)
 BYTES_IN_MB = 1024 * 1024
 
 
-def setup_logger(log_fpath: Path, level: int = logging.INFO) -> None:
+def setup_logger(log_fpath: Path | None = None, level: int = logging.INFO) -> None:
     """Initializes the logger with a file handler and a console handler."""
     log_formatter_file = logging.Formatter("%(asctime)s [%(levelname)-8s]  %(message)s")
     root_logger = logging.getLogger()
+
+    # ensuring no previous handler is active
+    while root_logger.hasHandlers():
+        root_logger.handlers[0].close  # noqa
+        root_logger.removeHandler(root_logger.handlers[0])
+
     root_logger.setLevel(level)
 
-    file_handler = logging.FileHandler(log_fpath, mode="w")
-    file_handler.setFormatter(log_formatter_file)
-    root_logger.addHandler(file_handler)
+    if log_fpath is not None:
+        file_handler = logging.FileHandler(log_fpath, mode="w")
+        file_handler.setFormatter(log_formatter_file)
+        root_logger.addHandler(file_handler)
 
     log_formatter_console = logging.Formatter("%(message)s")
     console_handler = logging.StreamHandler()
@@ -40,31 +47,30 @@ def download_zenodo_data(
     record_id: str, output_dir: Path, filenames: Collection[str] | None = None, *, cache_overwrite: bool = False
 ) -> None:
     output_dir.mkdir(parents=True, exist_ok=True)
-    try:
-        r = requests.get(f"https://zenodo.org/api/records/{record_id}", timeout=10)
-        r.raise_for_status()
 
-    # Catch all exceptions
-    except Exception as e:
-        logger.info(f"Failed to download Zenodo record {record_id}. Error: {e}")
+    if not cache_overwrite and filenames and all((output_dir / i).is_file() for i in filenames):
+        logger.info("All filenames are locally cached, no download necessary.")
+        return
 
-        if (output_dir / "SMARTEOLE-WFC-open-dataset.zip").exists():
-            logger.info("Using cached dataset.")
-            return
+    logger.info("Fetching data from zenodo...")
+    r = requests.get(f"https://zenodo.org/api/records/{record_id}", timeout=10)
+    r.raise_for_status()
+    remote_files: list[dict] = r.json()["files"]
 
-        raise
-
-    files_to_download = r.json()["files"]
-    if filenames is not None:
-        files_to_download = [i for i in files_to_download if i["key"] in set(filenames)]
-        if len(files_to_download) != len(filenames):
+    if filenames:
+        requested_filenames = set(filenames)
+        remote_filenames = {i["key"] for i in remote_files}
+        if not requested_filenames.issubset(remote_filenames):
             msg = (
-                f"Could not find all files in the Zenodo record. "
-                f"Missing files: {set(filenames) - {i['key'] for i in files_to_download}}"
+                "Could not find all files in the Zenodo record. "
+                f"Missing files: {requested_filenames.difference(remote_filenames)}"
             )
             raise ValueError(msg)
+        files_to_download = [i for i in remote_files if i["key"] in requested_filenames]
+    else:
+        files_to_download = remote_files
 
-    filepaths = []
+    downloaded_files = 0
     for file_to_download in files_to_download:
         dst_fpath = output_dir / file_to_download["key"]
         if not dst_fpath.exists() or cache_overwrite:
@@ -77,9 +83,11 @@ def download_zenodo_data(
                     chunk_number = chunk_number + 1
                     print(f"{chunk_number} out of {math.ceil(filesize)} MB downloaded", end="\r")
                     f.write(chunk)
+            downloaded_files += 1
         else:
             logger.info(f"File {dst_fpath} already exists. Skipping download.")
-        filepaths.append(dst_fpath)
+
+    logger.info(f"Download finished: {downloaded_files} new files cached.")
 
 
 def format_and_print_results_table(
