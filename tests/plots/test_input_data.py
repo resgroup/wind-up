cbe5f2126aab31ca457a7da4e99367e8f12b12d1
Samuel Northover-Naylor <Samuel.Northover-Naylor@res-group.com>
2025-04-14 13:05:01 +0100

    address PR comments

--- tests/plots/test_input_data.py
+++ tests/plots/test_input_data.py
@@ -1,4 +1,5 @@
 import copy
+import datetime as dt
 import logging
 import re
 
@@ -8,7 +9,9 @@ import pytest
 from matplotlib.testing.decorators import image_comparison
 
 from examples.smarteole_example import SmarteoleData
+from wind_up.constants import DataColumns
 from wind_up.interface import AssessmentInputs
+from wind_up.models import PrePost
 from wind_up.plots.input_data import plot_input_data_timeline
 
 logger = logging.getLogger(__name__)
@@ -85,8 +88,58 @@ class TestInputDataTimeline:
         assessment_inputs = copy.deepcopy(smarteole_assessment_inputs[0])
 
         # manual adjustments to the configuration for the test
+        # ----------------------------------------------------
         assessment_inputs.cfg.toggle = None
-        assessment_inputs.cfg.upgrade_first_dt_utc_start = pd.Timestamp("2020-03-01T00:00:00+0000")
+        assessment_inputs.cfg.years_for_lt_distribution = 5
+        assessment_inputs.cfg.years_for_detrend = 3
+        assessment_inputs.cfg.years_offset_for_pre_period = 1
+        assessment_inputs.cfg.upgrade_first_dt_utc_start = pd.Timestamp("2020-04-15T00:00:00+0000")
+        # analysis period
+        assessment_inputs.cfg.analysis_last_dt_utc_start = (
+            assessment_inputs.cfg.upgrade_first_dt_utc_start + pd.DateOffset(months=8)
+        )
+        assessment_inputs.cfg.analysis_first_dt_utc_start = (
+            assessment_inputs.cfg.analysis_last_dt_utc_start - pd.DateOffset(months=12)
+        )
+        # long-term period
+        assessment_inputs.cfg.lt_last_dt_utc_start = assessment_inputs.cfg.upgrade_first_dt_utc_start - dt.timedelta(
+            days=7
+        )  # go back 1 week for buffer before toggling
+        assessment_inputs.cfg.lt_first_dt_utc_start = assessment_inputs.cfg.lt_last_dt_utc_start - dt.timedelta(
+            days=(assessment_inputs.cfg.years_for_lt_distribution * 365.25),
+        )
+        # detrend period
+        assessment_inputs.cfg.detrend_last_dt_utc_start = assessment_inputs.cfg.lt_last_dt_utc_start
+        assessment_inputs.cfg.detrend_first_dt_utc_start = (
+            assessment_inputs.cfg.detrend_last_dt_utc_start
+            - dt.timedelta(days=assessment_inputs.cfg.years_for_detrend * 365.25)
+        )
+        # pre-post period
+        pre_post = PrePost(
+            post_first_dt_utc_start=assessment_inputs.cfg.upgrade_first_dt_utc_start,
+            post_last_dt_utc_start=assessment_inputs.cfg.analysis_last_dt_utc_start,
+            pre_first_dt_utc_start=assessment_inputs.cfg.upgrade_first_dt_utc_start
+            - dt.timedelta(days=(365.25 * assessment_inputs.cfg.years_offset_for_pre_period)),
+            pre_last_dt_utc_start=assessment_inputs.cfg.analysis_last_dt_utc_start
+            - dt.timedelta(days=(365.25 * assessment_inputs.cfg.years_offset_for_pre_period)),
+        )
+        assessment_inputs.cfg.prepost = pre_post
+        # extend scada data with fake time series data
+        d1 = assessment_inputs.cfg.lt_first_dt_utc_start
+        d2 = assessment_inputs.cfg.analysis_last_dt_utc_start
+        _df = pd.DataFrame(
+            columns=assessment_inputs.wf_df.columns,
+            index=pd.MultiIndex.from_product(
+                [
+                    list(assessment_inputs.wf_df.index.get_level_values(0).unique()),
+                    pd.date_range(start=d1, end=d2, freq=pd.Timedelta(minutes=10), tz="UTC"),
+                ],
+                names=["TurbineName", "TimeStamp_StartFormat"],
+            ),
+        )
+        _df["ActivePowerMean"] = 0.1
+        _df["YawAngleMean"] = 0.2
+        assessment_inputs.wf_df = _df
 
         assessment_inputs.cfg.yaw_data_exclusions_utc = [
             ("SMV1", pd.Timestamp("2020-03-01T00:00:00+0000"), pd.Timestamp("2020-03-03T00:00:00+0000")),
@@ -109,3 +162,41 @@ class TestInputDataTimeline:
             assessment_inputs.wf_df.loc[pd.IndexSlice[turbine_name, start:end], "YawAngleMean"] = np.nan
 
         plot_input_data_timeline(assessment_inputs)
+
+    def test_prepost_not_set_on_wu_cfg(
+        self, smarteole_assessment_inputs: tuple[AssessmentInputs, SmarteoleData]
+    ) -> None:
+        """Test that a ValueError is raised if prepost is not set on the configuration."""
+
+        assessment_inputs = copy.deepcopy(smarteole_assessment_inputs[0])
+        assessment_inputs.cfg.toggle = None  # ensure the analysis type is not toggle
+        assessment_inputs.cfg.prepost = None  # remove prepost from the configuration
+
+        with pytest.raises(ValueError, match="PrePost attribute is not set on WindUpConfig."):
+            plot_input_data_timeline(assessment_inputs)
+
+    @pytest.mark.slow
+    @pytest.mark.filterwarnings("ignore")
+    @image_comparison(
+        baseline_images=["input_data_timeline_fig_figsize"], remove_text=False, extensions=["png"], style="mpl20"
+    )
+    def test_figsize(self, smarteole_assessment_inputs: tuple[AssessmentInputs, SmarteoleData]) -> None:
+        assessment_inputs = copy.deepcopy(smarteole_assessment_inputs[0])
+
+        dfs = []
+        any_float = 0.1
+        n_turbines_to_add = 10
+        for t in range(n_turbines_to_add):
+            new_turbine = f"figsize_t0{t}"
+            _df = pd.DataFrame(
+                any_float,
+                index=assessment_inputs.wf_df.index.get_level_values(1),
+                columns=assessment_inputs.wf_df.columns,
+            )
+            _df[DataColumns.turbine_name] = new_turbine
+            _df = _df.reset_index().set_index([DataColumns.turbine_name, "TimeStamp_StartFormat"])
+            dfs.append(_df)
+
+        assessment_inputs.wf_df = pd.concat([assessment_inputs.wf_df, *dfs])
+
+        plot_input_data_timeline(assessment_inputs=assessment_inputs)
--- wind_up/plots/input_data.py
+++ wind_up/plots/input_data.py
@@ -2,10 +2,14 @@
 
 from __future__ import annotations
 
+import logging
 from enum import Enum
 from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+from wind_up.constants import DataColumns
 
 if TYPE_CHECKING:
     import datetime as dt
@@ -14,6 +18,9 @@ if TYPE_CHECKING:
     import pandas as pd
 
     from wind_up.interface import AssessmentInputs
+
+
+logger = logging.getLogger(__name__)
 
 
 class DateRangeColors(str, Enum):
@@ -31,8 +38,8 @@ def _validate_data_within_exclusions(
 ) -> None:
     _series = wf_series.copy()
     for exclusion in exclusions:
-        mask = _series.index.get_level_values("TurbineName") == exclusion[0]
-        _data = _series.loc[mask].droplevel(level="TurbineName", axis=0).sort_index()
+        mask = _series.index.get_level_values(DataColumns.turbine_name) == exclusion[0]
+        _data = _series.loc[mask].droplevel(level=DataColumns.turbine_name, axis=0).sort_index()
         if _data[exclusion[1] : exclusion[2]].notna().any():  # type: ignore[misc]
             _msg = f"Data is not all NaN within exclusion period {exclusion}"
             raise ValueError(_msg)
@@ -71,19 +78,19 @@ def _plot_data_coverage(
 def plot_input_data_timeline(
     assessment_inputs: AssessmentInputs,
     *,
-    figsize: tuple[int, int] = (12, 6),
-    height_ratios: tuple[int, int] = (2, 1),
+    figsize: tuple[int, int] | None = None,
+    height_ratios: tuple[int, int] | None = None,
     save_to_folder: Path | None = None,
-    scada_data_column_for_power: str = "ActivePowerMean",
-    scada_data_column_for_yaw_angle: str = "YawAngleMean",
+    scada_data_column_for_power: str = DataColumns.active_power_mean,
+    scada_data_column_for_yaw_angle: str = DataColumns.yaw_angle_mean,
 ) -> plt.Figure:
     """Plot timeline of input data with key milestones and data exclusions.
 
     This function does not do any data filtering itself, but instead only displays the data as it is provided.
 
     :param assessment_inputs: wind-up configuration and time series data for the assessment
-    :param figsize: size of the plot figure
-    :param height_ratios: height ratios for the two subplots
+    :param figsize: size of the plot figure, if `None` it will be auto-sized based on the number of turbines
+    :param height_ratios: ratios for the two subplots, if `None` it will be auto-sized based on the number of turbines
     :param save_to_folder: directory in which to save the plot
     :param scada_data_column_for_power: column name in the wind farm DataFrame to use for power data coverage plotting
     :param scada_data_column_for_yaw_angle: column name in the wind farm DataFrame to use for yaw data coverage plotting
@@ -98,38 +105,53 @@ def plot_input_data_timeline(
     _validate_data_within_exclusions([*_wu_cfg.exclusion_periods_utc], _df[pwr_col])
     _validate_data_within_exclusions([*_wu_cfg.yaw_data_exclusions_utc], _df[yaw_col])
 
-    fig, (ax_turbines, ax_wf) = plt.subplots(
-        ncols=1, nrows=2, sharex=True, figsize=figsize, gridspec_kw={"height_ratios": list(height_ratios)}
-    )
+    turbines = list(_df.index.get_level_values(DataColumns.turbine_name).unique())
+    n_turbines = len(turbines)
+    y_values = list(range(1, n_turbines + 1))
 
-    turbines = [t.name for t in _wu_cfg.asset.wtgs]
-    y_values = list(range(1, len(turbines) + 1))
+    if figsize is None:
+        logger.debug("Auto-sizing figure based on number of turbines")
+        figsize = (15, n_turbines)
+        if height_ratios is None:
+            height_ratios = (max(int(np.floor(n_turbines / 3)), 2), 1)
+
+    fig, (ax_turbines, ax_wf) = plt.subplots(
+        ncols=1,
+        nrows=2,
+        sharex=True,
+        figsize=figsize,
+        gridspec_kw={"height_ratios": list(height_ratios)},  # type:ignore[arg-type]
+    )
 
     for y_value_count, t in enumerate(turbines):
         y_value = y_value_count + 1
 
         # turbine power data coverage
         pwr_label = f"{pwr_col} is not NaN" if y_value_count == 0 else None
-        turbine_series_power = (_df.query(f"TurbineName == '{t}'").droplevel(level="TurbineName", axis=0).sort_index())[
-            pwr_col
-        ]
+        turbine_series_power = (
+            _df.query(f"{DataColumns.turbine_name} == '{t}'")
+            .droplevel(level=DataColumns.turbine_name, axis=0)
+            .sort_index()
+        )[pwr_col]
         _plot_data_coverage(
             turbine_series=turbine_series_power,
             ax=ax_turbines,
-            y_value=y_value - 0.1,
+            y_value=y_value - 0.1,  # to offset the power data from the yaw data on the axis
             color="darkblue",
             label=pwr_label,
         )
 
         # turbine yaw data coverage
         yaw_label = f"{yaw_col} is not NaN" if y_value_count == 0 else None
-        turbine_series_yaw = (_df.query(f"TurbineName == '{t}'").droplevel(level="TurbineName", axis=0).sort_index())[
-            yaw_col
-        ]
+        turbine_series_yaw = (
+            _df.query(f"{DataColumns.turbine_name} == '{t}'")
+            .droplevel(level=DataColumns.turbine_name, axis=0)
+            .sort_index()
+        )[yaw_col]
         _plot_data_coverage(
             turbine_series=turbine_series_yaw,
             ax=ax_turbines,
-            y_value=y_value + 0.1,
+            y_value=y_value + 0.1,  # to offset the yaw data from the power data on the axis
             color="lightskyblue",
             label=yaw_label,
         )
@@ -191,21 +213,23 @@ def plot_input_data_timeline(
     ]
 
     if _wu_cfg.toggle is None:
-        key_dates_styles = [
-            {
-                "trace_fmt": {"height": 0.5, "color": DateRangeColors.POST},
-                "label": "Post-Upgrade Period",
-                "left": _wu_cfg.upgrade_first_dt_utc_start,
-                "right": _wu_cfg.analysis_last_dt_utc_start,
-            },
-            {
-                "trace_fmt": {"height": 0.5, "color": DateRangeColors.PRE},
-                "label": "Pre-Upgrade Period",
-                "left": _wu_cfg.analysis_first_dt_utc_start,
-                "right": _wu_cfg.upgrade_first_dt_utc_start,
-            },
-            *key_dates_styles,
-        ]
+        if _wu_cfg.prepost is None:
+            _msg = "PrePost attribute is not set on WindUpConfig."
+            raise ValueError(_msg)
+        pre_style = {
+            "trace_fmt": {"height": 0.5, "color": DateRangeColors.PRE},
+            "label": "Pre-Upgrade Period",
+            "left": _wu_cfg.prepost.pre_first_dt_utc_start,
+            "right": _wu_cfg.prepost.pre_last_dt_utc_start,
+        }
+        post_style = {
+            "trace_fmt": {"height": 0.5, "color": DateRangeColors.POST},
+            "label": "Post-Upgrade Period",
+            "left": _wu_cfg.prepost.post_first_dt_utc_start,
+            "right": _wu_cfg.prepost.post_last_dt_utc_start,
+        }
+
+        key_dates_styles = [post_style, pre_style, *key_dates_styles]
     else:
         key_dates_styles = [
             {
@@ -235,14 +259,6 @@ def plot_input_data_timeline(
     )
     ax_wf.set_xlim(x_min, x_max)  # type: ignore[arg-type]
 
-    # legends
-    for a in [ax_turbines, ax_wf]:
-        # Shrink current axis's width by 20%
-        box = a.get_position()
-        a.set_position([box.x0, box.y0, box.width * 0.8, box.height])  # type: ignore[arg-type]
-        handles, labels = a.get_legend_handles_labels()
-        a.legend(handles[::-1], labels[::-1], bbox_to_anchor=(1.04, 1), borderaxespad=0)
-
     ax_turbines.set_title("Turbine Level")
     ax_turbines.set_ylabel("Turbine")
     ax_turbines.set_yticks(y_values)
@@ -256,6 +272,14 @@ def plot_input_data_timeline(
     ax_wf.set_xlabel("TimeStamp")
 
     fig.suptitle("Wind-Up Assessment Timeline", fontsize=14)
+
+    # legends
+    for a in [ax_turbines, ax_wf]:
+        # Shrink current axis's width by 20%
+        box = a.get_position()
+        a.set_position([box.x0, box.y0, box.width * 0.8, box.height])  # type: ignore[arg-type]
+        handles, labels = a.get_legend_handles_labels()
+        a.legend(handles[::-1], labels[::-1], bbox_to_anchor=(1.04, 1), borderaxespad=0)
 
     if save_to_folder is not None:
         if not save_to_folder.is_dir():
