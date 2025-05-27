5d9f5eb0ec5d072765382d6a4541bbcf750da738
Alex Clerc <alex.clerc@res-group.com>
2025-05-27 18:42:07 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime as dt
 from pathlib import Path
 
 import matplotlib as mpl
@@ -15,24 +16,39 @@ TEST_CONFIG_DIR = TEST_DATA_FLD / "config"
 CACHE_DIR = PROJECTROOT_DIR / "cache"
 
 
+def _set_legacy_datetimes(cfg: WindUpConfig) -> None:
+    """Set the datetimes as they were prior to v0.3."""
+    cfg.prepost.pre_first_dt_utc_start = cfg.upgrade_first_dt_utc_start - dt.timedelta(
+        days=(cfg.years_offset_for_pre_period * 365.25)
+    )
+    cfg.prepost.pre_last_dt_utc_start = cfg.analysis_last_dt_utc_start - dt.timedelta(
+        days=(cfg.years_offset_for_pre_period * 365.25)
+    )
+    cfg.lt_last_dt_utc_start = cfg.prepost.pre_last_dt_utc_start
+    cfg.lt_first_dt_utc_start = cfg.lt_last_dt_utc_start - dt.timedelta(days=(cfg.years_for_lt_distribution * 365.25))
+    cfg.detrend_last_dt_utc_start = cfg.lt_last_dt_utc_start
+    cfg.detrend_first_dt_utc_start = cfg.detrend_last_dt_utc_start - dt.timedelta(days=(cfg.years_for_detrend * 365.25))
+    return cfg
+
+
 @pytest.fixture
 def test_lsa_t13_config() -> WindUpConfig:
-    return WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_LSA_T13.yaml")
+    return _set_legacy_datetimes(WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_LSA_T13.yaml"))
 
 
 @pytest.fixture
 def test_brt_t16_pitch() -> WindUpConfig:
-    return WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_BRT_T16_pitch.yaml")
+    return _set_legacy_datetimes(WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_BRT_T16_pitch.yaml"))
 
 
 @pytest.fixture
 def test_marge_config() -> WindUpConfig:
-    return WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_Marge.yaml")
+    return _set_legacy_datetimes(WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_Marge.yaml"))
 
 
 @pytest.fixture
 def test_homer_config() -> WindUpConfig:
-    cfg = WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_Homer.yaml")
+    cfg = _set_legacy_datetimes(WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_Homer.yaml"))
     t1lat = -58.60364145072843
     t1long = 103.6841410289202052
     t2lat = -58.60261454305449
@@ -46,7 +62,7 @@ def test_homer_config() -> WindUpConfig:
 
 @pytest.fixture
 def test_homer_with_t00_config() -> WindUpConfig:
-    cfg = WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_Homer.yaml")
+    cfg = _set_legacy_datetimes(WindUpConfig.from_yaml(TEST_CONFIG_DIR / "test_Homer.yaml"))
     t1lat = -58.60364145072843
     t1long = 103.6841410289202052
     t2lat = -58.60261454305449
